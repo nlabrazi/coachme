8c83a8b71d62a6b389a240fb04c84c11b5becbaf
Nabil Labrazi <na.labrazi@gmail.com>
2020-05-25 18:41:18 +0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    "/home" # Or :prefix_to_your_route
+  end
+
+  def after_sign_up_path_for(resource)
+    "/home" # Or :prefix_to_your_route
+  end
 end
