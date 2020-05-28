2b1fb7123062889c33f8e807150b0c61f045d21e
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-05-28 12:55:29 +0200

    revue dashboard historique

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,17 @@ class DashboardsController < ApplicationController
   # after_action :verify_authorized
 
   def dashboard
+    @coach_activities = current_user.coach_activities
+    @bookings = current_user.bookings
+
+
     if current_user.coach?
       if current_user.location.nil? || current_user.legal.nil? || current_user.licence.nil?
         redirect_to edit_user_registration_path(current_user)
       end
     end
+
+
   end
 
 
