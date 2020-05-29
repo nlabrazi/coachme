55879b277494b5082e67018578205d4df4beef45
Nabil Labrazi <na.labrazi@gmail.com>
2020-05-29 13:29:38 +0200

    ajout style map coach activities & some css

--- app/controllers/coach_activities_controller.rb
+++ app/controllers/coach_activities_controller.rb
@@ -4,39 +4,49 @@ class CoachActivitiesController < ApplicationController
     @coach_activities = CoachActivity.all
     if params[:query]
       @coach_activities = @coach_activities.search_by_activity(params[:query])
+          #test markers
+          @users = User.geocoded
+          @markers = @users.map do |user|
+            {
+              lat: user.latitude,
+              lng: user.longitude,
+              infoWindow: render_to_string(partial: "info_window", locals: { user: user })
+              #id: user.id
+            }
+          end
+        end
+      end
+
+      def new
+        @coach_activity = CoachActivity.new
+      end
+
+      def show
+        @coach_activity = CoachActivity.find(params[:id])
+        @booking = Booking.new
+        @bookings = Booking.where(coach_activity_id: @coach_activity.id)
+        @coach = @coach_activity.user
+        @bookings_datetime = @bookings.map do |booking|
+          {
+            from: booking.start_time,
+            to:   booking.end_time
+          }
+        end
+      end
+
+      def create
+        @coach_activity = CoachActivity.new(coach_activity_params)
+        @coach_activity.user = current_user
+        if @coach_activity.save
+          redirect_to coach_activity_path(@coach_activity)
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def coach_activity_params
+        params.require(:coach_activity).permit(:activity_id, :capacity, :start_date, :end_date, :price)
+      end
     end
-  end
-
-  def new
-    @coach_activity = CoachActivity.new
-  end
-
-  def show
-    @coach_activity = CoachActivity.find(params[:id])
-    @booking = Booking.new
-    @bookings = Booking.where(coach_activity_id: @coach_activity.id)
-    @coach = @coach_activity.user
-    @bookings_datetime = @bookings.map do |booking|
-      {
-        from: booking.start_time,
-        to:   booking.end_time
-      }
-    end
-  end
-
-    def create
-    @coach_activity = CoachActivity.new(coach_activity_params)
-    @coach_activity.user = current_user
-    if @coach_activity.save
-      redirect_to coach_activity_path(@coach_activity)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def coach_activity_params
-    params.require(:coach_activity).permit(:activity_id, :capacity, :start_date, :end_date, :price)
-  end
-end
