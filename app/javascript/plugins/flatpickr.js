90cd07dfb8e9fca4a4e00bf6fef77cc10cc92b01
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-06-02 11:01:39 +0200

    page prete manque popup et map

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,7 +3,7 @@ import flatpickr from "flatpickr"
 //import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 const initFlatPicker = () => {
-  const bookingForm = document.getElementById('booking-form-div');
+  const bookingForm = document.getElementById('form-booking');
 
   if (bookingForm) {
   //const deals = JSON.parse(bookingForm.dataset.bookings);
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
+  scope :by_coach_activity, -> (coach_activity) {
+  self.joins(:bookings).where('bookings = ?',booking)
+  }
+
   belongs_to :user
   belongs_to :coach_activity
   belongs_to :coach, class_name: "User"
