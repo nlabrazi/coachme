fd92801a6acb905d8d3d9c86e4f14b5dfafe3994
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-06-19 09:36:32 +0200

    En avant guingampn

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user # <== was if @user.persist? -- after using devise-ios-rails gem it failed to work
+    if @user
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
